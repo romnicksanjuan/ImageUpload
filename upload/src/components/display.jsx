3e0e8b21116300be91fe79d648180c5906b07d29
romnick <romnicksanjuan9@gmail.com>
2024-03-24 00:42:37 +0800

    hhaa

--- upload/src/components/display.jsx
+++ upload/src/components/display.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
+import styles from '../css/display.module.css'
 
 
 
@@ -26,75 +27,28 @@ const Display = () => {
 
 
     return (
-        <div style={{
+        <div className={styles.main} >
 
-            height: 'auto',
-            width: '800px',
-            maxWidthidth: '80%',
-            justifyContent: 'center',
+            <h2 className={styles.h2}>My Gallery</h2>
 
+            <div className={styles.second}>
 
-        }}>
-
-            <h2 style={{ textAlign: 'center' }}>My Gallery</h2>
-
-            <div style={{
-                background: 'whitesmoke',
-                display: 'grid',
-                gridTemplateColumns: 'repeat(4,1fr)',
-                // gridAutoRows:'100px',
-                // height: '100%',
-                // flexWrap: 'wrap',
-                // width:'auto',
-
-                // overflow: 'auto',
-                // position:'relative',
-                // width:'100%',
-                // height:'0',
-                padding: '10px',
-                gap: '5px',
-                margin: 'auto',
-                // padding: '10px',
-                maxWidth: '80%',
-                border: '1px solid white',
-                // background:'whitesmoke',
-                placeItems: 'center',
-                // objectFit:'contain'
-                // backgroundSize:'contain'
-
-
-            }}>
-
-                {image.map((img) => (
-                    <div style={{
-                        padding: '0',
-                        // margin: '0',
-                        // background:'red'
-                        height: '150px'
-
-                    }} key={img._id} onClick={() => handleClick(img._id)}>
-                        <li style={{
-                            listStyle: 'none',
-                        }} >
-                            <img style={{
-                                cursor: 'pointer',
-                                height: '150px',
-                                // maxHeighteight:'100px',
-                                width: '150px',
-                                objectFit: 'cover',
-                                padding: '0',
-                                margin: '0'
-                            }} src={img.image} alt="" />
-                        </li>
-                    </div>
-
+            
+            {
+                image.map(img => (
+                    <ul key={img._id}>
+                    <li>
+                        <img src={img.image} alt="" />
+                    </li>
+                    </ul>
                 ))
-                }
+                
+             }
+            
+                
             </div>
 
-            {
-                imageclick && <img src={imageclick} alt="" />
-            }
+            
         </div>
 
 
